d7945f9a3872d0403ac5e52344fc737561617a8b
Moises Botarro <moises.bottaro.silva@gmail.com>
2019-04-16 17:28:47 -0300

    add dataset read function

--- Projeto/dataset.py
+++ Projeto/dataset.py
@@ -1,5 +1,42 @@
 # This module is responsible for loading the data set and apply ao the data augmentation operations
 
+import os
+import os.path
+from skimage import data, io, filters
+
+from sys import platform as sys_pf
+if sys_pf == 'darwin':
+    import matplotlib
+    matplotlib.use("TkAgg")
+
 
 def print_data():
     print("Hello from dataset")
+
+
+def get_faces(path='.'):
+    """
+    get_faces returns the faces in the data set
+    @param path: the data set directory to read the data from. Each person must have a separate folder with
+    its corresponding faces
+    @return: a slice with each index pointing to a slice containing a person's faces
+    """
+
+    dataset = []
+
+    # Get all files in directory except hidden files
+    people = [file for file in os.listdir(path) if not file.startswith('.')]
+
+    for person in people:
+        person_path = os.path.join(path, person)
+
+        faces = os.listdir(person_path)
+        faces.sort()
+        dataset.append([])
+
+        for face in faces:
+            file = os.path.join(person_path, face)
+            img = io.imread(file)
+            dataset[-1].append(img)
+
+    return dataset
--- Projeto/main.py
+++ Projeto/main.py
@@ -1,4 +1,26 @@
-from dataset import print_data
+from dataset import get_faces
+from skimage import io
 
 
-print_data()
+orl_faces = get_faces("./datasets/OrlFaces20")
+icmc_faces = get_faces("./datasets/PessoasICMC")
+
+print("ORL FACES")
+print(len(orl_faces))
+
+for person in orl_faces:
+    print(len(person))
+
+# io.imshow(orl_faces[9][9])
+# io.show()
+
+print("ICMC FACES")
+print(len(icmc_faces))
+
+for person in icmc_faces:
+    print(len(person))
+    io.imshow(person[0])
+    io.show()
+
+# io.imshow(orl_faces[9][9])
+# io.show()
